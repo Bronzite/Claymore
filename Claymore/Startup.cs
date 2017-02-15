bb8155bef9c333b6e21e94680f3bb10484e56aeb
John Brewer <bronzite@gmail.com>
2017-02-15 18:36:08 -0500

    Initial Commit

---
+++ Claymore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Claymore.Startup))]
+namespace Claymore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
