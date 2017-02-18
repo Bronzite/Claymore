6117119de6880a5150c04bd2a36bd5330d08d222
John Brewer <bronzite@gmail.com>
2017-02-17 20:53:30 -0500

    Added EdgePoint Extension for Available Edge Points.

---
+++ Claymore/Models/EdgePointExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Claymore.Models
+{
+    public partial class EdgePoints
+    {
+        public int AvailablePoints
+        {
+            get { if (AllocatedXP.HasValue)
+                    return (int)(Math.Floor((double)(AllocatedXP.Value / 20)));
+                else
+                    return 0;
+            }
+        }
+    }
+}
